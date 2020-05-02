ed9a9c0b641b2b04b4b8133e50fcbb8d62e79378
grizzlyPi <lucasrodriguesdelima@rocketmail.com>
2020-05-02 19:20:39 -0300

    User insertion with POST

--- src/main/java/com/udemy/workshopmongodb/services/UserService.java
+++ src/main/java/com/udemy/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.udemy.workshopmongodb.domain.User;
+import com.udemy.workshopmongodb.dto.UserDTO;
 import com.udemy.workshopmongodb.repositories.UserRepository;
 import com.udemy.workshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO obj) {
+		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
 }

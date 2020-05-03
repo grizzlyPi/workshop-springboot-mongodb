fb41bdbd625d35ebebda8b761aca56be332682b9
grizzlyPi <lucasrodriguesdelima@rocketmail.com>
2020-05-03 12:30:19 -0300

    Getting a post by id

---
+++ src/main/java/com/udemy/workshopmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.udemy.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.workshopmongodb.domain.Post;
+import com.udemy.workshopmongodb.repositories.PostRepository;
+import com.udemy.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id){
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}	
+}

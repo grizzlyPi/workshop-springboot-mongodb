f4391cefbf14a71b8ddeab0965d1aad20d712f3d
grizzlyPi <lucasrodriguesdelima@rocketmail.com>
2020-05-03 12:10:26 -0300

    Endpoint return a user's posts

--- src/main/java/com/udemy/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/udemy/workshopmongodb/resources/UserResource.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.udemy.workshopmongodb.domain.Post;
 import com.udemy.workshopmongodb.domain.User;
 import com.udemy.workshopmongodb.dto.UserDTO;
 import com.udemy.workshopmongodb.services.UserService;
@@ -61,5 +62,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping(value = "{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts()) ;
 	}
 }

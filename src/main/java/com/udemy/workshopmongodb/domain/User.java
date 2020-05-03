1854a250eb052fcdf0d9ea023917b7f9e74632f7
grizzlyPi <lucasrodriguesdelima@rocketmail.com>
2020-05-02 22:24:50 -0300

    Referencing user posts

--- src/main/java/com/udemy/workshopmongodb/domain/User.java
+++ src/main/java/com/udemy/workshopmongodb/domain/User.java
@@ -1,8 +1,11 @@
 package com.udemy.workshopmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -15,8 +18,10 @@ public class User implements Serializable {
 	private String name;
 	private String email;
 	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+	
 	public User() {
-		
 	}
 
 	public User(String id, String name, String email) {
@@ -48,6 +53,10 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public List<Post> getPosts(){
+		return posts;
 	}
 
 	@Override
